fe3f77da2a997dbca3aee199e7a67b51256eaa96
Kyle Tsuyemura <kt.fremont@gmail.com>
2016-06-30 13:10:44 -0700

    testing deployed social media

--- server/config/auths.js
+++ server/config/auths.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1874757336084999', // your App ID
         'clientSecret'  : 'b5ac25d341d1fe688fc153ec0445bc17', // your App Secret
-        'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
+        'callbackURL'   : 'http://54.183.209.215/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : '5P6t0RBgM9UtJtMxUdyO5Xg5K',
         'consumerSecret'    : 'uBdmqBiaxaYHlvGdkosNwqUcmM7I07tWdLl518NZzuHNjxCefC',
-        'callbackURL'       : 'http://localhost:8000/auth/twitter/callback'
+        'callbackURL'       : 'http://54.183.209.215/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '875266097111-4fiss65a6hrl12lq2ivsrbsakov432tf.apps.googleusercontent.com',
         'clientSecret'  : 'mQrtJ7RwFQySQCBQe455DdoM',
-        'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+        'callbackURL'   : 'http://54.183.209.215/auth/google/callback'
     }
 
 };
